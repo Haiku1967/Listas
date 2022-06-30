3b16018b5793436b87e41f6458c2e7981593bb4d
Haiku1967 <haiku_1967@hotmail.com>
2022-06-30 08:29:18 +0200

    feat: Botones CANCEL & NEXT

--- app/src/main/java/com/haiku1967/listas/LoginFormFragment.kt
+++ app/src/main/java/com/haiku1967/listas/LoginFormFragment.kt
@@ -1,9 +1,11 @@
 package com.haiku1967.listas
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.jocnunez.listas.R
 
@@ -14,6 +16,18 @@ class LoginFormFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login_form, container, false)
+        val view = inflater.inflate(R.layout.fragment_login_form, container, false)
+        val nextButton = view.findViewById<Button>(R.id.nextButton)
+
+        val cancelButton = view.findViewById<Button>(R.id.cancelButton)
+
+        nextButton.setOnClickListener {
+            Log.d("Debug", "Click on next button")
+        }
+        cancelButton.setOnClickListener {
+            Log.d("Debug", "Click on cancel button")
+        }
+        return view
     }
+
 }
