831d3eff37996c9c780c138c43f590f74ad0dfb3
Haiku1967 <haiku_1967@hotmail.com>
2022-07-01 13:50:48 +0200

    feat: Botones SAVE, CANCEL, DELETE

--- app/src/main/java/com/haiku1967/listas/ItemDetailFragment.kt
+++ app/src/main/java/com/haiku1967/listas/ItemDetailFragment.kt
@@ -1,10 +1,15 @@
 package com.haiku1967.listas
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.jocnunez.listas.R
 
 class ItemDetailFragment : Fragment() {
@@ -14,6 +19,33 @@ class ItemDetailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_item_detail, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_item_detail, container, false)
+
+
+        val cancelButton = view.findViewById<Button>(R.id.cancelItemButton)
+        val deleteButton = view.findViewById<Button>(R.id.deleteItemButton)
+        val saveButton = view.findViewById<Button>(R.id.saveItemButton)
+
+        cancelButton.setOnClickListener { cancelButtonHandler(view) }
+        deleteButton.setOnClickListener { deleteButtonHandler(view) }
+        saveButton.setOnClickListener { saveButtonHandler(view) }
+        return view
+    }
+
+    fun cancelButtonHandler(view: View) {
+
+        val title = view.findViewById<TextInputEditText>(R.id.titleField)
+        val description = view.findViewById<TextInputEditText>(R.id.descriptionField)
+
+        title.setText("")
+        description.setText("")
+
+    }
+    fun deleteButtonHandler(view: View) {
+
+    }
+        fun saveButtonHandler(view: View) {
+
     }
 }
